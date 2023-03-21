ed54e4329366ce0d2ededb11b99808b206ae0b75
silviu48 <silviu48vatamanelu@gmail.com>
2023-03-21 11:54:45 +0100

    dist

---
+++ dist/main.js
@@ -0,0 +1,96 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/drawing.ts":
+/*!************************!*\
+  !*** ./src/drawing.ts ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"Drawing\": () => (/* binding */ Drawing)\n/* harmony export */ });\nvar Drawing = /** @class */ (function () {\n    function Drawing() {\n        var _this = this;\n        this.options = {\n            color: \"black\",\n            lineWidth: 2\n        };\n        this.root = null;\n        this.canvas = null;\n        this.context = null;\n        this.seletedColor = this.options.color;\n        this.lineWidth = this.options.lineWidth;\n        this.drawingFromX = null;\n        this.drawingFromY = null;\n        this.drawingToX = null;\n        this.drawingToY = null;\n        this.mouseDown = false;\n        this.addListeners = function () {\n            document.addEventListener(\"DOMContentLoaded\", function () {\n                console.log(\"LOADED\");\n                _this.root = document.body;\n                _this.canvas = _this.getCanvas();\n            });\n        };\n        console.log(\"Drawing constructor\");\n        this.addListeners();\n    }\n    Drawing.prototype.getCanvas = function () {\n        if (this.canvas != null) {\n            return this.canvas;\n        }\n        var controlsHeight = document.getElementById(\"controls\").offsetHeight;\n        this.canvas = document.createElement(\"canvas\");\n        this.canvas.width = window.innerWidth;\n        this.canvas.height = window.innerHeight - controlsHeight - 20;\n        this.root.prepend(this.canvas);\n    };\n    return Drawing;\n}());\n\n\n\n//# sourceURL=webpack://my-webpack-project/./src/drawing.ts?");
+
+/***/ }),
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _drawing__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./drawing */ \"./src/drawing.ts\");\n\nvar drawing = new _drawing__WEBPACK_IMPORTED_MODULE_0__.Drawing();\n\n\n//# sourceURL=webpack://my-webpack-project/./src/index.ts?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.ts");
+/******/ 	
+/******/ })()
+;
